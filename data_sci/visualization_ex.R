2dbeaba491012c60666c63312b8b039a8af34665
Rebekah Kim <rkim3737@gmail.com>
2016-12-08 01:12:18 -0500

    Working on Viz
    *need to explain group = 1 setting

--- data_sci/visualization_ex.R
+++ data_sci/visualization_ex.R
@@ -55,10 +55,106 @@ ggplot(data = mpg) + geom_point(mapping = aes(x = displ, y = hwy)) + facet_grid(
 
 
 ## Geometric objects
-ggplot(data = mpg) + geom_smooth(mapping = aes(x = displ, y = hwy, linetype = drv))
+ggplot(data = mpg) + 
+    geom_point(
+      mapping = aes(x = displ, y = hwy)
+    ) +
+    geom_smooth(
+      mapping = aes(x = displ, y = hwy, linetype = drv)
+    )
+
+# same as
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
+  geom_point() +
+  geom_smooth(
+    mapping = aes(group = drv)
+  )
+
+#filtering data
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
+  geom_point(mapping = aes(color = class)) +
+  # filter data
+  geom_smooth(data = filter(mpg, class == "subcompact"), se = F)
+
+#line chart
+ggplot(data = filter(mpg, year == 1999), mapping = aes(x = displ, y = hwy)) +
+  geom_line()
+
+#boxplot
+ggplot(data = mpg, mapping = aes(x = factor(year), y = displ)) +
+  geom_boxplot()
+
+#histogram
+ggplot(data = mpg, mapping = aes(x = hwy)) +
+  geom_histogram(binwidth = 1)
+
+#area chart
+ggplot(data = mpg, mapping = aes(x = displ, fill = factor(cyl))) +
+  geom_area(stat="bin", binwidth = 0.25)
+  
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy, color = drv)) +
+  geom_point() +
+  geom_smooth(se = F) +
+  # remove color legend
+  #guides(color = F)
+  #remove all legend
+  theme(legend.position="none")
+  
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
+  geom_point() +
+  #geom_smooth(color = "blue", se =F)
+  geom_smooth(aes(group = drv), color = "blue", se= F)
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
+  geom_point(aes(color = drv)) +
+  #geom_smooth(color = "blue", se =F)
+  #geom_smooth(se= F)
+  geom_smooth(aes(linetype = drv), se= F)
+
+#statistical transformations
+str(diamonds)
+
+#bar graph
+ggplot(data = diamonds)+
+  geom_bar(mapping =aes(x = cut))
+  #same as
+  stat_count(aes(x=cut))
+
+#looking at proportions
+ggplot(data = diamonds) +
+  geom_bar(mapping= aes(x = cut, y =..prop.., group =1))
+
+#stat summary
+ggplot(data = diamonds) +
+  stat_summary(
+    mapping= aes(x=cut, y= depth),
+      fun.ymin = min,
+      fun.ymax = max,
+      fun.y = median,
+      geom = "crossbar"
+  )
+
+#geom_col for having heights of bars that represent values in data
+ggplot(data = diamonds, aes(x = cut, y = carat))+
+  geom_col() # ??? quite not sure what happens exactly
+
+#unfinished exercise
+#??? Most geoms and stats come in pairs that are almost always used in concert. 
+#??? Read through the documentation and make a list of all the pairs. What do they have in common?
+
+ggplot(data = diamonds, aes(x = carat, y = log(price)))+
+  geom_point()+
+  geom_smooth()
+  #stat_smooth()
+
+#need to set group = 1 because (see below 2 plots)
+ggplot(data = diamonds) + 
+  geom_bar(mapping = aes(x = cut, y = ..prop..))
+ggplot(data = diamonds) + 
+  geom_bar(mapping = aes(x = cut, fill = color, y = ..prop..))
 
 
-
+#group `````````````
 
 
 
